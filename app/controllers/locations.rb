a744e0290672f0fac522ac1fa2be3e871c5635d8
Lydia & Tim Kelly <pair+lydiamnash.timlkelly@devbootcamp.com>
2016-03-18 10:38:56 -0500

    add create and delete on user page

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -1,12 +1,28 @@
 get '/locations/:id' do
   @saved_location = SavedLocation.find(params[:id])
-
   long = @saved_location.location.longitude.to_s
   lat = @saved_location.location.latitude.to_s
-  url = "https://api.breezometer.com/baqi/?lat=" + lat + "&lon=" + long + "&key=56f593e1971d4690b3b16b3523f9c7b4"
-  # p url
+  url = "http://api.breezometer.com/baqi/?lat=" + lat + "&lon=" + long + "&key=56f593e1971d4690b3b16b3523f9c7b4"
 
-  # @response = HTTParty.get(url)
-
+  # HTTParty::Basement.default_options.update(verify: false) # Use this if http request include ssl
+  @api_response = HTTParty.get(url)
+  p @api_response
   erb :'/locations/show'
+end
+
+post '/locations' do
+  new_location = Location.find_or_create_by(params[:loc])
+
+  saved_location = SavedLocation.new(params[:new_saved_location])
+  saved_location.location = new_location
+  saved_location.user = current_user
+  if saved_location.save
+    redirect '/users'
+  end
+end
+
+delete '/locations/:id' do
+  location = SavedLocation.find(params[:id])
+  location.destroy
+  redirect '/users'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 tim   = User.create!({first_name: "tim", last_name: "kelly", email: "timlkelly@gmail.com", password: "password"})
 lydia = User.create!({first_name: "lydia", last_name: "nash", email: "lydiamnash@gmail.com", password: "password"})
 
-chicago  = Location.create!({latitude: 41.8369,  longitude: 87.6847})
-berkeley = Location.create!({latitude: 122.2728, longitude: 37.8717})
-oakland  = Location.create!({latitude: 122.2708, longitude: 37.8044})
-london   = Location.create!({latitude: 0.1275,   longitude: 51.5072})
-evanston = Location.create!({latitude: 87.6947,  longitude: 42.0464})
+chicago  = Location.create!({latitude: 41.8369,  longitude: -87.6847})
+berkeley = Location.create!({latitude: 37.8717,  longitude: -122.2728})
+oakland  = Location.create!({latitude: 37.8044,  longitude: -122.2708})
+london   = Location.create!({latitude: 51.5072,  longitude: -0.1275})
+evanston = Location.create!({latitude: 42.0464,  longitude: -87.6947})
 
 assocation  = SavedLocation.create!({ location_id: 1, user_id: 1, name: "Home" })
 assocation1 = SavedLocation.create!({ location_id: 4, user_id: 1, name: "Study Abroad" })
