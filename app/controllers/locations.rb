30284c11f1fdaaf93311f4d619843643f4aaad50
Lydia & Tim Kelly <pair+lydiamnash.timlkelly@devbootcamp.com>
2016-03-18 12:53:06 -0500

    Add working d3

--- app/controllers/locations.rb
+++ app/controllers/locations.rb
@@ -5,8 +5,19 @@ get '/locations/:id' do
   url = "http://api.breezometer.com/baqi/?lat=" + lat + "&lon=" + long + "&key=56f593e1971d4690b3b16b3523f9c7b4"
 
   # HTTParty::Basement.default_options.update(verify: false) # Use this if http request include ssl
-  @api_response = HTTParty.get(url)
-  p @api_response
+  api_response = HTTParty.get(url)
+
+  # Create hash using desired api data
+  @aq_data = Hash.new
+  @aq_data["aqi"] = api_response.parsed_response["breezometer_aqi"]
+  @aq_data["aqi_color"] = api_response.parsed_response["breezometer_color"]
+  @aq_data["aqi_description"] = api_response.parsed_response["breezometer_description"]
+  @aq_data["dom_poll"] = api_response.parsed_response["dominant_pollutant_canonical_name"]
+
+  @aq_data_json = @aq_data.to_json
+
+
+
   erb :'/locations/show'
 end
 
